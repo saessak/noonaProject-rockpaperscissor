54da7c061925dbfc671a756300fc98db5811b4a0
dootopia <dootopia00@gmail.com>
2022-05-23 14:48:04 +0900

    add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
+import Main from './pages/Main';
 import Rockpaperscissor from './pages/Rockpaperscissor';
 import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
@@ -13,7 +13,7 @@ export default function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Main />}></Route>
           <Route path="/Rockpaperscissor" element={<Rockpaperscissor />}></Route>
           <Route path="/Page2" element={<Page2 />}></Route>
           <Route path="/Page3" element={<Page3 />}></Route>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,13 +2,13 @@
 import Header from '../component/Header';
 
 // 메인 페이지
-export default function Home() {
+export default function Main() {
 
     return (
 
         <div>
             <Header/>
-            <div>안녕하세요</div>
+            <div>안녕하세요. 메인페이지입니다</div>
         </div>
     );
 }
